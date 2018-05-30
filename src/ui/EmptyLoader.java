86e9cd8a88a89462360536aee8b908fffe1fd2df
kxmo <kxmo@users.noreply.github.com>
2018-05-30 14:26:10 +1200

    Add EmptyLoader for AssetLoaders that will not throw IOException
    These loaders may include those that load no assets at all, or those
    that generate their own assets in ways that cannot fail

---
+++ src/ui/EmptyLoader.java
@@ -0,0 +1,30 @@
+package ui;
+
+import java.io.IOException;
+
+import main.interfaces.AssetLoader;
+import ui.interfaces.Displayable;
+
+/**
+ * An AssetLoader that does not load any assets, but instead
+ * returns the provided Displayable.
+ * This should be used in place of one's own implementation
+ * of this class if they intend to load no assets. This loader
+ * can also be used if assets are generated in a way that cannot
+ * fail.
+ */
+public class EmptyLoader implements AssetLoader
+{
+	private final Displayable d;
+	
+	public EmptyLoader(Displayable d)
+	{
+		this.d = d;
+	}
+	
+	@Override
+	public Displayable loadDisplayer() throws IOException
+	{
+		return d;
+	}
+}
