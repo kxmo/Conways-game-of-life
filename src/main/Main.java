eef6df9d9d99c37d594679af71a01a73651f9600
kxmo <kxmo@users.noreply.github.com>
2018-06-12 14:17:06 +1200

    Add main parsing arguments
    Main now parses -l taking the loader as an argument. The remaining
    arguments are passed to the loader

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import datastructures.Pair;
 import main.interfaces.AssetLoader;
+import parsing.terminal.Parser;
+import ui.gui.GraphicalAssetLoader;
 import ui.interfaces.Displayable;
 import ui.terminal.CommandLineAssetLoader;
 
@@ -12,18 +16,47 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Pair<Optional<Displayable>, Optional<String>> p = loadDisplayable(new CommandLineAssetLoader(), args);
-		
-		Optional<Displayable> d = p.left();
-		Optional<String> e = p.right();
-		
-		if (d.isPresent())
+		Parser mainArgs = new Parser();
+		mainArgs.addOption("l", "loader", "The name of the asset loader", true, true);
+
+		Pair<Map<String, String>, String[]> toLoadPair = mainArgs.parseReturnUnused(args);
+		Map<String, String> toLoad = toLoadPair.left();
+		String[] remainingArgs = toLoadPair.right();
+
+		Map<String, AssetLoader> available = getAvailableAssetLoaders();
+
+		if (toLoad.containsKey("loader") && available.containsKey(toLoad.get("loader")))
 		{
-			d.get().run();
+			AssetLoader loader = available.get(toLoad.get("loader"));
+			Pair<Optional<Displayable>, Optional<String>> p = loadDisplayable(loader, remainingArgs);
+			runUIOrExit(p.left(), p.right());
 		}
-		else if (e.isPresent())
+		else
 		{
-			System.err.println(e.get());
+			System.err.println(mainArgs.getHelpMessage());
+			System.exit(3);
+		}
+	}
+
+	private static Map<String, AssetLoader> getAvailableAssetLoaders()
+	{
+		Map<String, AssetLoader> loaders = new HashMap<>();
+
+		loaders.put("cli", new CommandLineAssetLoader());
+		loaders.put("gui", new GraphicalAssetLoader());
+
+		return loaders;
+	}
+
+	private static void runUIOrExit(Optional<Displayable> userInterface, Optional<String> error)
+	{
+		if (userInterface.isPresent())
+		{
+			userInterface.get().run();
+		}
+		else if (error.isPresent())
+		{
+			System.err.println(error.get());
 			System.exit(1);
 		}
 		else
@@ -32,7 +65,7 @@ public class Main
 			System.exit(2);
 		}
 	}
-	
+
 	private static Pair<Optional<Displayable>, Optional<String>> loadDisplayable(AssetLoader loader, String[] args)
 	{
 		try
